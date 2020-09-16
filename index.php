62f0400241fccf0bf122c1c4cefa2b7ae98d84a6
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-16 21:37:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <?php
   error_reporting(E_ALL);
   ini_set('display_errors', 1);  
+  exec("kill $(lsof -t -i:9515)"); 
   //exec("php panther.php");
   require 'vendor/autoload.php';
   echo("mnames");
