f08b74cc7456126fc1ca0939ce9b92bfaab1c23a
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-16 21:49:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,15 @@
 </head>
 <body>
     <p>hello there, matthew here</p>
-
+<?php
+  error_reporting(E_ALL);
+  ini_set('display_errors', 1);  
+  exec("kill $(lsof -t -i:9515)"); 
+  //exec("php panther.php");
+  require 'vendor/autoload.php';
+  echo("mnames");
+  $client = \Symfony\Component\Panther\Client::createChromeClient();  
+  $client->request('GET', 'https://www.youtube.com/');
+?>
 </body>
 </head>
