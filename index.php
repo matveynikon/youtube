8d5043c66b46ce226eef7ab7e83fce44993f9f3e
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-16 21:17:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,16 +6,6 @@
 </head>
 <body>
     <p>hello there, matthew here</p>
-<?php
-  error_reporting(E_ALL);
-  ini_set('display_errors', 1);  
-  require 'vendor/autoload.php';
-  echo("mnames");
-  $client = \Symfony\Component\Panther\Client::createChromeClient();
-    //use Goutte\Client;
-    //$client = new Goutte\Client();    
-  $client->request('GET', 'https://matveynikon.wixsite.com/dailyhero/post/4-ocean');
-  sleep(10);
-?>
+
 </body>
 </head>
