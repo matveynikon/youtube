4fdda454212d645deb30dcf62fa55fbcdcaf338f
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-17 22:00:10 +0200

    Update panther.php

--- panther.php
+++ panther.php
@@ -1,5 +1,5 @@
   require 'vendor/autoload.php';
   echo("mnames");
   $client = \Symfony\Component\Panther\Client::createChromeClient();  
-  $client->request('GET', 'https://www.youtube.com/');
-  sleep(10);
+  $client->request('GET', 'https://matveynikon.wixsite.com/dailyhero/post/4-ocean');
+  sleep(5);
