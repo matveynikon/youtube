7c1059542817b226b6fad46dc911dfcc089ff70e
matveynikon <66773302+matveynikon@users.noreply.github.com>
2020-09-19 09:24:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,12 +9,11 @@
 <?php
   error_reporting(E_ALL);
   ini_set('display_errors', 1);  
-  exec("cd vendor/symfony/panther/chromedriver-bin/"); 
-  sleep(1);
-  exec("./update.sh");
+  //exec("cd vendor/symfony/panther/chromedriver-bin/"); 
+  //exec("./update.sh");
   require 'vendor/autoload.php';
   echo("mnames");
-  $client = \Symfony\Component\Panther\Client::createChromeClient(null, [
+  $client = \Symfony\Component\Panther\Client::createFirefoxClient(null, [
       '--no-sandbox'      
   ]);  
   $client->request('GET', 'https://matveynikon.wixsite.com/dailyhero/post/4-ocean');
